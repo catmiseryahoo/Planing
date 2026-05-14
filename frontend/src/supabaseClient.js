0daf0aaf16077fa2109c5786d4b0f54958165182
catmiseryahoo <catmiser@yahoo.com>
2026-05-14 10:45:39 +0500

    Hide Supabase keys in env

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wqfpksyemvaxncsqwuzm.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxZnBrc3llbXZheG5jc3F3dXptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNDUyNTIsImV4cCI6MjA5MzYyMTI1Mn0.J1C1b0uw2PGJMWu-zh-CJ3xdKtWOYb6XNJH4-4nTzcQ';
-const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxZnBrc3llbXZheG5jc3F3dXptIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODA0NTI1MiwiZXhwIjoyMDkzNjIxMjUyfQ.sDIeIT4ndken6-XeA_VzR2eM0Do9W9EpfggLnL5IPcU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseServiceKey = import.meta.env.VITE_SUPABASE_SERVICE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
